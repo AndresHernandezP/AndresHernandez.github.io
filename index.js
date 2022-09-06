12e5cb8c661ad59c6d16d8bdd54158c6d025e478
NoobieDs <106280861+NoobieDs@users.noreply.github.com>
2022-09-05 19:25:00 -0500

    final update
    masterbranch-finalupdate

---
+++ index.js
@@ -0,0 +1,33 @@
+function show() {
+    alert("This ain´t for real fool");
+  }
+function cambio1(){
+    document.getElementById('page1').style.backgroundImage="url(https://i.ibb.co/9wMjqQ8/fondofinal3.png)"
+    document.getElementById('page1').style.backgroundSize="100%"
+    document.getElementById('page1').style.backgroundPosition="80% 80%"
+    document.getElementById('page1').style.backgroundRepeat="no-repeat"
+    var element = document.getElementById("Title2");
+  element.innerHTML = "actividad para niños";
+  document.getElementById('Title2').setAttribute('style', 'color: rgba(96,140,122,255)'); 
+  document.getElementById('BaB').style.bgcolor='#c28b00'
+}
+function cambio2(){
+  document.getElementById('page1').style.backgroundImage="url(https://i.ibb.co/BCrhTLQ/comidafinal.png)"
+    document.getElementById('page1').style.backgroundSize="100%"
+    document.getElementById('page1').style.backgroundPosition="80% 80%"
+    document.getElementById('page1').style.backgroundRepeat="no-repeat"
+    var element = document.getElementById("Title2");
+  element.innerHTML = "idea para una cena especial";
+  document.getElementById('Title2').setAttribute('style', 'color: rgba(194,139,0,255)'); 
+}
+function cambio3(){
+  document.getElementById('page1').style.backgroundImage="url(https://i.ibb.co/kqp2HZR/bricolagefinal.png)"
+    document.getElementById('page1').style.backgroundSize="100%"
+    document.getElementById('page1').style.backgroundPosition="80% 80%"
+    document.getElementById('page1').style.backgroundRepeat="no-repeat"
+    var element = document.getElementById("Title2");
+  element.innerHTML = "proyecto de bricolage";
+  document.getElementById('Title2').setAttribute('style', 'color:rgba(96,140,122,255)'); 
+}
+
+
